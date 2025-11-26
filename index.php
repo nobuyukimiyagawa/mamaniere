67b2b444a46d4d4d3a2412443e7360750c3081f3
宮川宣之 <78245918+nobuyukimiyagawa@users.noreply.github.com>
2025-11-26 11:32:51 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,170 @@
+<?php include 'header.php'; ?>
+
+<section class="top_fv">
+  <div class="swiper" id="top_fv_slider">
+    <div class="swiper-wrapper">
+
+      <!-- Slide 1 -->
+      <div class="swiper-slide">
+        <div class="top_fv_image">
+          <div class="top_fv_image_fixed_bg">
+            <div class="top_fv_image_fixed_img" role="img" aria-label="赤ちゃんを抱っこする母親"
+              data-swiper-parallax="-23%"
+              style="background-image: url('/assets/images/fv_img01.png');">
+            </div>
+          </div>
+        </div>
+        <div class="top_fv_text">
+          <p>いつもがんばっている<br>ママをサポートする<br>産後ケアサイト<br>「mamaniere」へようこそ！<br>充実の育児タイムを<br>mamaniereとともに。</p>
+        </div>
+      </div>
+
+      <!-- Slide 2 -->
+      <div class="swiper-slide">
+        <div class="top_fv_image">
+          <div class="top_fv_image_fixed_bg">
+            <div class="top_fv_image_fixed_img" role="img" aria-label="こどもに勉強を教える母親"
+              data-swiper-parallax="-23%"
+              style="background-image: url('/assets/images/fv_img02.png');">
+            </div>
+          </div>
+        </div>
+        <div class="top_fv_text">
+          <p>こんな時にはセルフチェックで<br>自身の状態を把握しましょう！</p>
+          <ul>
+            <li>育児をより快適にしたい時</li>
+            <li>お子様の成長によって<br>生活習慣が変わってきた時</li>
+            <li>漠然とした不安や焦りが<br>消えない時</li>
+          </ul>
+        </div>
+      </div>
+
+      <!-- Slide 3 -->
+      <div class="swiper-slide">
+        <div class="top_fv_image">
+          <div class="top_fv_image_fixed_bg">
+            <div class="top_fv_image_fixed_img" role="img" aria-label="赤ちゃんのお世話をする母親"
+              data-swiper-parallax="-23%"
+              style="background-image: url('/assets/images/fv_img03.png');">
+            </div>
+          </div>
+        </div>
+        <div class="top_fv_text">
+          <p>育児に役立つサービスや<br>情報を活用して<br>心身ともに健やかな育児を。<span>by POLA</span></p>
+        </div>
+      </div>
+
+      <!-- Slide 4 -->
+      <div class="swiper-slide">
+        <div class="top_fv_image">
+          <div class="top_fv_image_fixed_bg">
+            <div class="top_fv_image_fixed_img" role="img" aria-label="ママニエールイラスト"
+              data-swiper-parallax="-23%"
+              style="background-image: url('/assets/images/fv_img04.png');">
+            </div>
+          </div>
+        </div>
+        <div class="top_fv_text">
+          <p>今、気づいてほしい。<br>あなたに秘められた<br>大きな愛と、可能性に。
+            <span class="mt05">mamaniereが全てのママに贈る<br>ショートムービー。</span>
+          </p>
+          <div class="youtube_icon">
+            <a href="#" target="_blank">
+              <img src="/assets/images/youtube_icon.png" alt="YouTube">
+            </a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  <h1>
+    <img src="/assets/images/logo.png" alt="ママニエール">
+  </h1>
+
+  <div class="fv_links_list">
+    <a class="fv_links_item" href="/analysis/">
+      <div class="fv_links_item_content">
+        <p>育児の節目に心身の状態をチェック！<br>ママのコンディションも大切に。</p>
+      </div>
+      <div class="fv_links_item_title">
+        <p>ママの体調をチェック</p>
+      </div>
+    </a>
+
+    <a class="fv_links_item" href="/service/">
+      <div class="fv_links_item_content">
+        <p>ママをサポート＆育児に役立つ<br>さまざまなサービスをご紹介！</p>
+      </div>
+      <div class="fv_links_item_title">
+        <p>サービスをさがす</p>
+      </div>
+    </a>
+
+    <a class="fv_links_item" href="/column/">
+      <div class="fv_links_item_content">
+        <p>スペシャリストのアドバイスや<br>親子の体験アクティビティをシェア。</p>
+      </div>
+      <div class="fv_links_item_title">
+        <p>コラムを読む</p>
+      </div>
+    </a>
+  </div>
+
+  <div class="scroll_down">
+    <p>More Information</p>
+    <img src="/assets/images/scroll_arrow.png" alt="スクロールアイコン">
+  </div>
+</section>
+
+<section class="top_about">
+  <div class="inner">
+    <div class="about_text">
+      <p>毎日十分すぎるくらい、がんばっているママへ。<br>
+        ママ自身のことも大切にして欲しい<br>
+        そんな想いから生まれた「ママニエール」。<br>
+        独自の分析システムでママの“今”を見つめ、<br>
+        分析結果に基づいた、多彩な情報をご案内。<br>
+        「ママニエール」は、いつだってママの味方です。
+      </p>
+    </div>
+    <div class="btn"><a href="#">mamaniere詳細はこちら</a></div>
+  </div>
+</section>
+
+<section class="top_banner">
+  <div class="inner">
+    <div class="banner_image">
+      <a href="/column/">
+        <img src="/assets/images/top_bnr01.png" alt="産後サポートコラム">
+      </a>
+
+      <a href="/care/">
+        <img src="/assets/images/top_bnr02.png" alt="美容・育児等お役立ち情報">
+      </a>
+    </div>
+  </div>
+</section>
+
+<section class="top_youtube">
+  <div class="inner">
+    <div class="youtube_content">
+      <div class="youtube_text">
+        <h2>Short Movie</h2>
+        <p>今、気づいてほしい。<br>
+          あなたに秘められた<br>
+          大きな愛と、可能性に。<br>
+          <span>mamaniereが全てのママに贈る<br>ショートムービー。</span>
+        </p>
+      </div>
+      <div class="youtube_img">
+        <a href="#" target="_blank">
+          <img src="/assets/images/youtube_img.jpg" alt="YouTube">
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php include 'footer.php'; ?>
